99584dd975e1d0e180d3bd8ce8e7062fbec959b0
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2020-08-20 17:29:09 +0200

    feat: Add cooking recipes

---
+++ src/recipes/Cooking.ts
@@ -0,0 +1,56 @@
+import * as t from "io-ts";
+import { ItemOrTag, ItemOrTags, Item } from "../parts";
+import { Ingredient, toIngredients, stringify } from "./common";
+import { pipe } from "fp-ts/lib/pipeable";
+import { Either, chain } from "fp-ts/lib/Either";
+
+export type OwnCooking = t.TypeOf<typeof OwnCooking>;
+export interface MCCooking {
+	type: "blasting" | "campfire_cooking" | "smelting" | "smoking";
+	ingredient: Ingredient;
+	experience: number;
+	cookingtime: number;
+	result: string;
+}
+
+const OwnCooking = t.type({
+	type: t.keyof({
+		blasting: null,
+		campfire_cooking: null,
+		smelting: null,
+		smoking: null,
+	}),
+	ingredients: t.union([ItemOrTag, ItemOrTags]),
+	experience: t.Int,
+	cookingtime: t.Int,
+	result: Item,
+});
+
+function encode(x: OwnCooking): MCCooking {
+	return {
+		type: x.type,
+		ingredient: toIngredients(x.ingredients),
+		experience: x.experience,
+		cookingtime: x.cookingtime,
+		result: stringify(x.result),
+	};
+}
+
+function validate(u: unknown, c: t.Context): Either<t.Errors, OwnCooking> {
+	return pipe(
+		OwnCooking.validate(u, c),
+		chain(x => {
+			if (x.cookingtime <= 0) {
+				return t.failure(u, c, "cookingtime must be a positive integer.");
+			}
+
+			if (x.experience <= 0) {
+				return t.failure(u, c, "experience must be a positive integer.");
+			}
+
+			return t.success(x);
+		}),
+	);
+}
+
+export const Cooking = new t.Type<OwnCooking, MCCooking, unknown>("Cooking", OwnCooking.is, validate, encode);

9c092dc36b69e7d9eddc895f46f342cac8e5fdf6
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2020-08-20 16:55:32 +0200

    feat: Add shaped crafting recipes

---
+++ src/recipes/CraftingShaped.test.ts
@@ -0,0 +1,143 @@
+import * as assert from "assert";
+import { isRight } from "fp-ts/lib/Either";
+import { CraftingShaped } from "./CraftingShaped";
+import { items, item, tag } from "../parts";
+import { itemIng, tagIng } from "./common";
+
+describe("Shaped crafting", () => {
+	test("invalid inputs", () => {
+		// Empty pattern
+		expect(
+			CraftingShaped.decode({
+				type: "crafting_shaped",
+				pattern: [],
+				key: {},
+				result: "1 apple",
+			}),
+		).toBeLeft();
+
+		// Pattern rows are uneven
+		expect(
+			CraftingShaped.decode({
+				type: "crafting_shaped",
+				pattern: ["c", "cc"],
+				key: { c: "cobblestone" },
+				result: "2 cobblestone_stairs",
+			}),
+		).toBeLeft();
+
+		// Pattern too short
+		expect(
+			CraftingShaped.decode({
+				type: "crafting_shaped",
+				pattern: [""],
+				key: {},
+				result: "1 apple",
+			}),
+		).toBeLeft();
+
+		// Pattern too wide
+		expect(
+			CraftingShaped.decode({
+				type: "crafting_shaped",
+				pattern: ["aaaaa"],
+				key: { a: "apple" },
+				result: "1 apple",
+			}),
+		).toBeLeft();
+
+		// Pattern too tall
+		expect(
+			CraftingShaped.decode({
+				type: "crafting_shaped",
+				pattern: ["a", "a", "a", "a"],
+				key: { a: "apple" },
+				result: "1 apple",
+			}),
+		).toBeLeft();
+
+		// Entry in pattern is missing in key
+		expect(
+			CraftingShaped.decode({
+				type: "crafting_shaped",
+				pattern: ["ab", "ab"],
+				key: { a: "apple" },
+				result: "1 apple",
+			}),
+		).toBeLeft();
+
+		// Entry in key is missing in pattern
+		expect(
+			CraftingShaped.decode({
+				type: "crafting_shaped",
+				pattern: ["a"],
+				key: { a: "apple", c: "cobblestone" },
+				result: "1 apple",
+			}),
+		).toBeLeft();
+
+		// Key isn't a single character
+		expect(
+			CraftingShaped.decode({
+				type: "crafting_shaped",
+				pattern: ["ac"],
+				key: { a: "apple", ac: "cobblestone" },
+				result: "1 apple",
+			}),
+		).toBeLeft();
+	});
+
+	test("crafting table", () => {
+		const recipe = CraftingShaped.decode({
+			type: "crafting_shaped",
+			pattern: ["pp", "pp"],
+			key: { p: "+planks" },
+			result: "1 crafting_table",
+		});
+
+		expect(recipe).toBeRight({
+			type: "crafting_shaped",
+			pattern: ["pp", "pp"],
+			key: { p: tag("planks") },
+			result: items("crafting_table", 1),
+		});
+
+		assert(isRight(recipe));
+		expect(CraftingShaped.encode(recipe.right)).toEqual({
+			type: "crafting_shaped",
+			pattern: ["pp", "pp"],
+			key: { p: [tagIng("planks")] },
+			result: {
+				count: 1,
+				item: "minecraft:crafting_table",
+			},
+		});
+	});
+
+	test("enchanting table", () => {
+		const recipe = CraftingShaped.decode({
+			type: "crafting_shaped",
+			pattern: [" b ", "dod", "ooo"],
+			key: { b: "book", d: "diamond", o: "obsidian" },
+			result: "1 enchanting_table",
+		});
+
+		expect(recipe).toBeRight({
+			type: "crafting_shaped",
+			pattern: [" b ", "dod", "ooo"],
+			key: { b: item("book"), d: item("diamond"), o: item("obsidian") },
+			result: items("enchanting_table", 1),
+		});
+
+		assert(isRight(recipe));
+		expect(CraftingShaped.encode(recipe.right)).toEqual({
+			type: "crafting_shaped",
+			pattern: [" b ", "dod", "ooo"],
+			key: { b: [itemIng("book")], d: [itemIng("diamond")], o: [itemIng("obsidian")] },
+			result: {
+				count: 1,
+				item: "minecraft:enchanting_table",
+			},
+		});
+	});
+});
---
+++ src/recipes/CraftingShaped.ts
@@ -0,0 +1,91 @@
+import * as t from "io-ts";
+import { Items, ItemOrTag, ItemOrTags } from "../parts";
+import { Ingredient, toIngredients, stringify } from "./common";
+import { Either, chain } from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/pipeable";
+
+export type OwnCraftingShaped = t.TypeOf<typeof OwnCraftingShaped>;
+export interface MCCraftingShaped {
+	type: "crafting_shaped";
+	pattern: string[];
+	key: Record<string, Ingredient>;
+	result: {
+		count: number;
+		item: string;
+	};
+}
+
+const OwnCraftingShaped = t.type({
+	type: t.literal("crafting_shaped"),
+	pattern: t.array(t.string),
+	key: t.record(t.string, t.union([ItemOrTag, ItemOrTags], "ItemOrTag | Array<ItemOrTag>")),
+	result: Items,
+});
+
+function encode(x: OwnCraftingShaped): MCCraftingShaped {
+	const keyRecord: MCCraftingShaped["key"] = {};
+
+	for (const key of Object.keys(x.key)) {
+		keyRecord[key] = toIngredients(x.key[key]);
+	}
+
+	return {
+		type: x.type,
+		pattern: x.pattern,
+		key: keyRecord,
+		result: {
+			count: x.result.count,
+			item: stringify(x.result),
+		},
+	};
+}
+
+function validate(u: unknown, c: t.Context): Either<t.Errors, OwnCraftingShaped> {
+	return pipe(
+		OwnCraftingShaped.validate(u, c),
+		chain(x => {
+			if (x.pattern.length === 0 || x.pattern.length > 3) {
+				return t.failure(u, c, "Pattern height must be 1-3.");
+			}
+
+			const columns = x.pattern[0].length;
+			if (!x.pattern.every(row => row.length === columns)) {
+				return t.failure(u, c, "All strings in pattern must have same length.");
+			}
+
+			if (columns === 0 || columns > 3) {
+				return t.failure(u, c, "Pattern width must be 1-3.");
+			}
+
+			const charsInPattern = new Set<string>();
+
+			for (const row of x.pattern) {
+				for (const char of row) {
+					if (char !== " " && !(char in x.key)) {
+						return t.failure(u, c, `Unknown key found in pattern: "${char}".`);
+					}
+					charsInPattern.add(char);
+				}
+			}
+
+			for (const key of Object.keys(x.key)) {
+				if (key.length !== 1) {
+					return t.failure(u, c, `Key doesn't have length of 1: "${key}".`);
+				}
+
+				if (!charsInPattern.has(key)) {
+					return t.failure(u, c, `"${key}" exists in key but not in pattern.`);
+				}
+			}
+
+			return t.success(x);
+		}),
+	);
+}
+
+export const CraftingShaped = new t.Type<OwnCraftingShaped, MCCraftingShaped, unknown>(
+	"CraftingShaped",
+	OwnCraftingShaped.is,
+	validate,
+	encode,
+);
